3c5fa2cb37bd4886007e28ba41db42ba05aa28e2
kurousa <r.takahashi.1026@gmail.com>
2022-10-15 14:37:48 +0900

    header implemets: show amazon logo

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import Image from "next/image"
+
+function Header() {
+  return (
+    <header>
+      {/* Top Nav */}
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+
+        {/* Logo */}
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+          <Image
+           src="https://links.papareact.com/f90"
+           width={150}
+           height={40}
+           objectFit="contain"
+           className="cursor-pointer"
+          />
+        </div>
+
+        {/* Search Bar*/}
+
+      </div>
+      {/* Bottom Nav */}
+      <div>
+
+      </div>
+    </header>
+  )
+}
+
+export default Header
