645af4f5e06cf2b352217439c22436ba1d86a499
kurousa <r.takahashi.1026@gmail.com>
2022-10-15 15:16:32 +0900

    implements SearchBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,9 @@
-import Image from "next/image"
+import Image from "next/image";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon
+} from "@heroicons/react/outline";
 
 function Header() {
   return (
@@ -18,6 +23,13 @@ function Header() {
         </div>
 
         {/* Search Bar*/}
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+          <input 
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4" 
+            type="text" 
+          />
+          <SearchIcon className="h-12 p-4"/>
+        </div>
 
       </div>
       {/* Bottom Nav */}
