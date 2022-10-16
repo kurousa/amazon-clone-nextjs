2a08b22df3b4b334ff2afac60c331a287856a7a8
kurousa <r.takahashi.1026@gmail.com>
2022-10-16 16:06:12 +0900

    add routing, counting baske item from redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,19 @@ import {
   SearchIcon,
   ShoppingCartIcon
 } from "@heroicons/react/outline";
-import { 
-  signIn, 
-  signOut, 
-  useSession 
+import {
+  signIn,
+  signOut,
+  useSession
 } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { selectItems } from "../slices/basketSlice";
 
 function Header() {
   const session = useSession();
+  const router = useRouter();
+  const items = useSelector(selectItems);
 
   return (
     <header>
@@ -21,25 +26,28 @@ function Header() {
         {/* Logo */}
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
-           src="https://links.papareact.com/f90"
-           width={150}
-           height={40}
-           objectFit="contain"
-           className="cursor-pointer"
+            onClick={() => router.push("/")}
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+            objectFit="contain"
+            className="cursor-pointer"
           />
         </div>
 
         {/* Search Bar*/}
         <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
-          <input 
+          <input
             className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4" 
-            type="text" 
+            type="text"
           />
           <SearchIcon className="h-12 p-4"/>
         </div>
 
         {/* Right Side Menu */}
         <div className="text-white flex items-center text-xs space-x-6 mx-6">
+
+          {/* User Info */}
           <div onClick={!session.data ? signIn : signOut} className="link">
             <p className="hover:underline">
               {session.data ? `Hello, ${session.data?.user.name}` : "Sign In"}
@@ -47,15 +55,21 @@ function Header() {
             <p className="font-extrabold md:text-sm">Account & List</p>
           </div>
 
+          {/* Order History */}
           <div className="link">
             <p>Returns</p>
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
 
-          <div className="relative link flex items-center">
+          {/* Basket */}
+          <div 
+            onClick={()=>router.push("/checkout")}
+            className="relative link flex items-center"
+          >
             {/* Number of in the basket*/}
-            {/* TODO: count up with redux */}
-            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
+              {items.length}
+            </span>
             <ShoppingCartIcon className="h-10" />
             <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
           </div>
