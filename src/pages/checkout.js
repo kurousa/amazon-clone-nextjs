385c0981055ef371bc2fae1b361ee1caa775ebd1
kurousa <r.takahashi.1026@gmail.com>
2022-10-22 22:38:08 +0900

    implements checkout with stripe.

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -7,11 +7,35 @@ import Currency from 'react-currency-formatter';
 import {
   useSession
 } from "next-auth/react";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+const stripePromise = loadStripe(process.env.stripe_public_key);
 
 function Checkout() {
   const { data: session } = useSession();
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
+
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise;
+    // Call the backend to create a checkout session.
+    const checkoutSession = await axios.post(
+      '/api/create-checkout-session',
+      {
+        items: items,
+        email: session?.user?.email
+      }
+    )
+
+    // Redirce user/customer to Stripe Checkout
+    const result = await stripe.redirectToCheckout({
+      sessionId: checkoutSession.data.id
+    })
+
+    if (result.error) {
+      alert(result.error.message);
+    }
+  }
 
   return (
     <div className="bg-gray-100">
@@ -64,6 +88,8 @@ function Checkout() {
             </h2>
 
             <button 
+              role='link'
+              onClick={createCheckoutSession}
               disabled={!session}
               className={`button mt-2 ${!session && `from-gray-300 to-gray-500 border-gray-200 text-gray-300 active:from-gray-300 cursor-not-allowed`}`}
             >
