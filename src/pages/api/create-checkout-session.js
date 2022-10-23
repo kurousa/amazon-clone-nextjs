78694f3daf695c6cbe53b32957ca598775e512ec
kurousa <r.takahashi.1026@gmail.com>
2022-10-23 12:45:06 +0900

    use NEXTAUTH_URL as HOST

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -21,8 +21,8 @@ export default async (req, res) => {
     payment_method_types:["card"],
     line_items: transformedItems,
     mode: 'payment',
-    success_url: `${process.env.HOST}/success`,
-    cancel_url: `${process.env.HOST}/checkout`,
+    success_url: `${process.env.NEXTAUTH_URL}/success`,
+    cancel_url: `${process.env.NEXTAUTH_URL}/checkout`,
     metadata: {
       email,
       images: JSON.stringify(items.map(item => item.image))
